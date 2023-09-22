66d602ee2222f58c86a0835357b6ef5ba92fed15
sponser <sponser@life.localdomain>
2023-09-23 00:42:56 +0300

    K8s files updates

--- PsqlFiles/init.sql
+++ PsqlFiles/init.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE todotasks;
-ALTER ROLE postgres SET client_encoding TO 'utf8';
-ALTER ROLE postgres SET default_transaction_isolation TO 'read committed';
-ALTER ROLE postgres SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE todotasks TO postgres;
+CREATE USER sponser WITH PASSWORD 'sponser';
+ALTER ROLE sponser SET client_encoding TO 'utf8';
+ALTER ROLE sponser SET default_transaction_isolation TO 'read committed';
+ALTER ROLE sponser SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE todotasks TO sponser;
