e68831bf023c3c8a7d5baf131676c747e7d5210d
sponser <sponser@life.localdomain>
2023-09-23 20:03:47 +0300

    K8s files changes

--- delete_components.sh
+++ delete_components.sh
@@ -3,7 +3,7 @@ kubectl delete service dj-svc psql-svc
 kubectl delete configmap dj-config-map
 kubectl delete secret dj-secret
 echo "[+] Waiting for Pods termination ..."
-sleep 20
-docker images --filter="reference=dj-img" --quiet | xargs docker rmi 
+sleep 25
+docker images --filter="reference=dj-img" --quiet | xargs docker rmi 2>dev/null
 docker images --filter="reference=psql-img" --quiet | xargs docker rmi
 echo "[+] K8s components have been deleted successfully."
